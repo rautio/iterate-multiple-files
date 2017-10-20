4f7215c997b83e9c8038df63c8cb0892c04273fc
Oskari_Rautiainen <oskari_rautiainen@symantec.com>
2017-10-20 14:37:26 -0700

    Added functionality to show code coverage with istanbul and automation for coveralls with travis

--- test/iterate.spec.js
+++ test/iterate.spec.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import expect from 'expect';
 import iterate from '../src/iterate.js';
 
-describe('Verify that iterate() works with matching file sizes', function(){
+describe('.iterate(files, operation).then', function(){
   const str1 = '1\n2\n3\n4\n';
   const str2 = '5\n6\n7\n8\n';
   const str3 = '9\n10\n11\n12\n';
@@ -15,7 +15,7 @@ describe('Verify that iterate() works with matching file sizes', function(){
   const expected = [15,18,21,24];
   const expectedTotal = 78;
   let total = 0;
-  it('Should return an array of the sums',function(){
+  it('Should return an array of sums',function(){
     return iterate(filePaths, function(lines, lineNr){
       //end of line or mismatching file lengths will return blank or null respectively
       //We added an end of line new-line to our files above
@@ -39,7 +39,7 @@ describe('Verify that iterate() works with matching file sizes', function(){
         expect(actual).toEqual(expected);
       });
   });
-  it('Should return a total sum of ' + expectedTotal, function(){
+  it('Should have calculated a total sum of ' + expectedTotal, function(){
     expect(total).toEqual(expectedTotal);
   });
   //Cleanup
